9b55f62116adbeb2544d8b57492d7736f5e2e18e
Dan Grigsby <dan.grigsby@wildflowerschools.org>
2017-08-22 13:58:07 -0500

    testing code for heroku

--- app/authorize_qbo_blueprint/config/default.py
+++ app/authorize_qbo_blueprint/config/default.py
@@ -2,5 +2,3 @@ import os
 
 QBO_CONSUMER_KEY = os.environ['QBO_CONSUMER_KEY']
 QBO_CONSUMER_SECRET = os.environ['QBO_CONSUMER_SECRET']
-
-#
--- app/authorize_qbo_blueprint/models.py
+++ app/authorize_qbo_blueprint/models.py
@@ -11,8 +11,8 @@ qbo = OAuth().remote_app(
     request_token_url = 'https://oauth.intuit.com/oauth/v1/get_request_token',
     access_token_url  = 'https://oauth.intuit.com/oauth/v1/get_access_token',
     authorize_url     = 'https://appcenter.intuit.com/Connect/Begin',
-    consumer_key      = app.config['QBO_CONSUMER_KEY'],
-    consumer_secret   = app.config['QBO_CONSUMER_SECRET']
+    consumer_key      = "TMP", # app.config['QBO_CONSUMER_KEY'],
+    consumer_secret   = "TMP", #app.config['QBO_CONSUMER_SECRET']
 )
 
 @qbo.tokengetter
