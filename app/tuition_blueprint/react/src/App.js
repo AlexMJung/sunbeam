75aa77dbf6e9d41875230aaa9953cae13d5d2802
Dan Grigsby <dan.grigsby@wildflowerschools.org>
2017-10-02 10:12:47 -0500

    simple customers

--- app/tuition_blueprint/react/src/App.js
+++ app/tuition_blueprint/react/src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class Customers extends Component {
   constructor(props) {
     super(props);
+    this.state = { customers: [] };
     var that = this;
     fetch("http://localhost:5000/tuition/customers", {credentials: 'include'})
       .then(function(response) {
-        that.customers = response.json()
-      })
-    fetch("http://localhost:5000/tuition/items", {credentials: 'include'})
-      .then(function(response) {
-        that.items = response.json()
-      })
-    fetch("http://localhost:5000/tuition/recurring_payments", {credentials: 'include'})
-      .then(function(response) {
-        that.recurring_payments = response.json()
-      })
+        return response.json()
+      }).then(function(parsed_json) {
+        that.setState({customers: parsed_json})
+      });
   }
   render() {
     return (
-      <div className="App">
+      <div className="Customers">
+        {this.state.customers.map(
+          function(customer){
+            return <p>{customer.name}</p>;
+          }
+        )}
+      </div>
+    )
+  }
+}
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Customers/>
       </div>
     );
   }
